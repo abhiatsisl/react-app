6462d9ec3b31ee5fc45a7d4322677bae4df1b845
Abhishek Jaiswal <abhishek_jaiswal@sislinfotech.com>
2021-12-02 17:36:40 +0530

    fix:route issue

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './index.css';
 import Header from './pages/index';
 import Page from './pages/page';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Header />
-    <Page/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Header/>} />
+        <Route path="/page" element={<Page/>} />
+      </Routes>
+    </Router >
+  )
+}
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,document.getElementById('root'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
