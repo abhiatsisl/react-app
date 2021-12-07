a8480f0b44dc9844452ebd4be34c154adfbfafb8
Abhishek Jaiswal <abhishek_jaiswal@sislinfotech.com>
2021-12-07 18:37:25 +0530

    fix:minor bugs fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ function Header(){
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav.Link href="/list">User list</Nav.Link>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
